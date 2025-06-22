a94499d390a146911e8f37871610337711805933
Jonathan0823 <eguinjonathan08@gmail.com>
2025-06-22 07:53:01 +0700

    feat(app): include navbar to main page

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,16 @@
+import gsap from "gsap";
 import "./App.css";
+import { ScrollTrigger, SplitText } from "gsap/all";
+import Navbar from "./components/Navbar";
+
+gsap.registerPlugin(ScrollTrigger, SplitText);
 
 function App() {
-  return <div>Text</div>;
+  return (
+    <main>
+      <Navbar />
+    </main>
+  );
 }
 
 export default App;
