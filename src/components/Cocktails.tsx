d1d366eadf9da2995ee2c618ef57b12fd583e6f2
Jonathan0823 <eguinjonathan08@gmail.com>
2025-06-22 17:57:39 +0700

    feat(cocktails): add content and animation to the leaves

--- src/components/Cocktails.tsx
+++ src/components/Cocktails.tsx
@@ -1,5 +1,81 @@
+import { useGSAP } from "@gsap/react";
+import { cocktailLists, mockTailLists } from "../constants";
+import gsap from "gsap";
+
 const Cocktails = () => {
-  return <section id="cocktails" className="noisy"></section>;
+  useGSAP(() => {
+    const parallaxTimeline = gsap.timeline({
+      scrollTrigger: {
+        trigger: "#cocktails",
+        start: "top 30%",
+        end: "bottom 80%",
+        scrub: true,
+      },
+    });
+
+    parallaxTimeline
+      .from("#c-left-leaf", {
+        x: -100,
+        y: 100,
+      })
+      .from("#c-right-leaf", {
+        x: 100,
+        y: 100,
+      });
+  }, []);
+  return (
+    <section id="cocktails" className="noisy h-dvh">
+      <img
+        src="/images/cocktail-left-leaf.png"
+        alt="left-leaf"
+        id="c-left-leaf"
+        className="c-left-leaf"
+      />
+      <img
+        src="/images/cocktail-right-leaf.png"
+        alt="right-leaf"
+        id="c-right-leaf"
+        className="c-right-leaf"
+      />
+
+      <div className="list">
+        <div className="popular">
+          <h2>List of Popular Cocktails</h2>
+          <ul>
+            {cocktailLists.map(({ name, country, detail, price }) => (
+              <li key={name}>
+                <div className="me-28">
+                  <h3>{name}</h3>
+                  <p>
+                    {country} | {detail}
+                  </p>
+                </div>
+                <span>- {price}</span>
+              </li>
+            ))}
+          </ul>
+          k
+        </div>
+        <div className="loved">
+          <h2>List of Loved Mocktails</h2>
+          <ul>
+            {mockTailLists.map(({ name, country, detail, price }) => (
+              <li key={name}>
+                <div className="me-28">
+                  <h3>{name}</h3>
+                  <p>
+                    {country} | {detail}
+                  </p>
+                </div>
+                <span>- {price}</span>
+              </li>
+            ))}
+          </ul>
+          k
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default Cocktails;
