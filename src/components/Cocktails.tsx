35f1c67936859daf303d9341c040f87300edfc2e
Jonathan0823 <eguinjonathan08@gmail.com>
2025-06-22 19:27:29 +0700

    feat(cocktails): update start animation

--- src/components/Cocktails.tsx
+++ src/components/Cocktails.tsx
@@ -18,7 +18,7 @@ const Cocktails = () => {
     const menuTimeline = gsap.timeline({
       scrollTrigger: {
         trigger: "#cocktails",
-        start: "top 50%",
+        start: "top 40%",
       },
     });
 
