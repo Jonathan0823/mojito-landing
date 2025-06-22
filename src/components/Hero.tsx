23086aceaa5d1e084ca59d9116c09f8359496847
Jonathan0823 <eguinjonathan08@gmail.com>
2025-06-22 09:32:05 +0700

    feat(hero): add video scroll animation

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,10 +1,15 @@
 import { useGSAP } from "@gsap/react";
 import gsap from "gsap";
 import { SplitText } from "gsap/all";
+import { useRef } from "react";
+import { useMediaQuery } from "react-responsive";
 
 gsap.registerPlugin(SplitText);
 
 const Hero = () => {
+  const videoRef = useRef<HTMLVideoElement>(null);
+  const isMobile = useMediaQuery({ maxWidth: 767 });
+
   useGSAP(() => {
     const heroSplit = new SplitText(".title", {
       type: "chars, words",
@@ -61,6 +66,27 @@ const Hero = () => {
         },
         0,
       );
+
+    const startValue = isMobile ? "top 50%" : "center 60%";
+    const endValue = isMobile ? "120% top" : "bottom top";
+
+    const tl = gsap.timeline({
+      scrollTrigger: {
+        trigger: "video",
+        start: startValue,
+        end: endValue,
+        scrub: true,
+        pin: true,
+      },
+    });
+
+    if (videoRef.current) {
+      videoRef.current.onloadedmetadata = () => {
+        tl.to(videoRef.current, {
+          currentTime: videoRef.current?.duration,
+        });
+      };
+    }
   }, []);
   return (
     <>
@@ -99,6 +125,15 @@ const Hero = () => {
           </div>
         </div>
       </section>
+      <div className="video absolute inset-0">
+        <video
+          ref={videoRef}
+          src="/videos/output.mp4"
+          muted
+          playsInline
+          preload="auto"
+        />
+      </div>
     </>
   );
 };
